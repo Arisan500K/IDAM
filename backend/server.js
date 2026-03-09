2948a8085ef05bbfba3d0539d1c8e28eb8cb228a
Arisan500K <daelishop28@gmail.com>
2026-03-09 22:01:17 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 
 const authRoutes = require("./routes/auth");
 const memberRoutes = require("./routes/members");
+const spinRoutes = require("./routes/spin");
 
 const app = express();
 const PORT = 3000;
@@ -12,12 +13,15 @@ app.use(express.json());
 
 app.use("/api", authRoutes);
 app.use("/api", memberRoutes);
+app.use("/api", spinRoutes);
 
 app.get("/", (req,res)=>{
+
 res.json({
 status:"Server Arisan 500K Aktif",
 versi:"1.0"
 });
+
 });
 
 app.listen(PORT,()=>{

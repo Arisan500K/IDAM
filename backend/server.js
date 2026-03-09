162d51bb074c588af25535cc5571cbcd9fbf0a95
Arisan500K <daelishop28@gmail.com>
2026-03-09 21:49:19 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,25 @@
 const express = require("express");
 const cors = require("cors");
 
+const authRoutes = require("./routes/auth");
+
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.json({
-        status: "Server Arisan 500K Aktif",
-        versi: "1.0"
-    });
+app.use("/api", authRoutes);
+
+app.get("/", (req,res)=>{
+
+res.json({
+status:"Server Arisan 500K Aktif",
+versi:"1.0"
 });
 
-app.listen(PORT, () => {
-    console.log("Server berjalan di http://localhost:" + PORT);
+});
+
+app.listen(PORT,()=>{
+console.log("Server berjalan di http://localhost:"+PORT);
 });

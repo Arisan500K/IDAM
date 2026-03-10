b20935ac5d4fb26e61540af296b8b6115898466e
Arisan500K <daelishop28@gmail.com>
2026-03-10 07:09:19 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,73 @@
+async function startSpin(){
 
+const {value:name}=await Swal.fire({
+
+title:"Pilih Nama Anda",
+input:"select",
+inputOptions:MEMBERS
+
+})
+
+if(!name) return
+
+animateSpin()
+
+setTimeout(()=>{
+
+let result=randomWinner()
+
+spinResults[name]=result
+
+voteData[result]=(voteData[result]||0)+1
+
+saveData()
+
+Swal.fire("Hasil Spin",result)
+
+renderAll()
+
+checkWinner()
+
+},4000)
+
+}
+
+function checkWinner(){
+
+const total=MEMBERS.length-1
+const done=Object.keys(spinResults).length
+
+if(done===total){
+
+let max=0
+let winner=""
+
+for(let n in voteData){
+
+if(voteData[n]>max){
+
+max=voteData[n]
+winner=n
+
+}
+
+}
+
+winnerData[winner]=getMonth()
+
+saveData()
+
+confetti()
+
+}
+
+}
+
+function renderAll(){
+
+renderStats()
+renderLeaderboard()
+
+}
+
+renderAll()

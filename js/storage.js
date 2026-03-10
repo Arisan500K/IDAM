3af213a888e0f927bb19d47d902cf05705f53e02
Arisan500K <daelishop28@gmail.com>
2026-03-10 07:08:05 +0700

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,13 @@
+let voteData = JSON.parse(localStorage.getItem("voteData")) || {}
+let spinResults = JSON.parse(localStorage.getItem("spinResults")) || {}
+let winnerData = JSON.parse(localStorage.getItem("winnerData")) || {}
+let payData = JSON.parse(localStorage.getItem("payData")) || {}
 
+function saveData(){
+
+localStorage.setItem("voteData",JSON.stringify(voteData))
+localStorage.setItem("spinResults",JSON.stringify(spinResults))
+localStorage.setItem("winnerData",JSON.stringify(winnerData))
+localStorage.setItem("payData",JSON.stringify(payData))
+
+}
